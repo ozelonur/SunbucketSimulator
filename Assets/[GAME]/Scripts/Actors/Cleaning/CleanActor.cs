ab3811defcafd62a043efb552c9b7afb0254f81d
onurozelpf <sumotoma_onur@hotmail.com>
2024-08-04 08:31:27 +0300

    Clean System

--- Assets/[GAME]/Scripts/Actors/Cleaning/CleanActor.cs
+++ Assets/[GAME]/Scripts/Actors/Cleaning/CleanActor.cs
@@ -3,6 +3,7 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
+using System.Collections;
 
 namespace DefaultNamespace
 {
@@ -11,6 +12,7 @@ namespace DefaultNamespace
         [Header("Configurations")] 
         [SerializeField] private Texture2D dirtMaskBase;
         [SerializeField] private Texture2D brush;
+        [SerializeField] private float calculationInterval = 1.0f; // Interval in seconds
 
         private Camera _mainCamera;
         private Texture2D _templateDirtMask;
@@ -18,11 +20,20 @@ namespace DefaultNamespace
         private NativeArray<Color32> _dirtMaskPixels;
         private NativeArray<Color32> _brushPixels;
 
+        private int _totalPixels;
+        private int _cleanedPixelsCount;
+        private bool _isDirty;
+        private bool _calculationStopped;
+
         private void Start()
         {
             _mainCamera = Camera.main;
             _renderer = GetComponent<MeshRenderer>();
             CreateTexture();
+            _totalPixels = _templateDirtMask.width * _templateDirtMask.height;
+
+            // Start the coroutine to calculate cleaned percentage at intervals
+            StartCoroutine(CalculateCleanedPercentageRoutine());
         }
 
         private void Update()
@@ -39,12 +50,11 @@ namespace DefaultNamespace
                     int pixelXOffset = Mathf.Clamp(pixelX - (brush.width / 2), 0, _templateDirtMask.width - brush.width);
                     int pixelYOffset = Mathf.Clamp(pixelY - (brush.height / 2), 0, _templateDirtMask.height - brush.height);
 
-                    var job = new ApplyBrushJob
+                    ApplyBrushJob job = new ApplyBrushJob
                     {
                         dirtMaskPixels = _dirtMaskPixels,
                         brushPixels = _brushPixels,
                         brushWidth = brush.width,
-                        brushHeight = brush.height,
                         maskWidth = _templateDirtMask.width,
                         pixelXOffset = pixelXOffset,
                         pixelYOffset = pixelYOffset
@@ -55,6 +65,9 @@ namespace DefaultNamespace
 
                     _templateDirtMask.SetPixels32(_dirtMaskPixels.ToArray());
                     _templateDirtMask.Apply();
+
+                    // Mark as dirty for recalculation
+                    _isDirty = true;
                 }
             }
         }
@@ -77,6 +90,46 @@ namespace DefaultNamespace
             _renderer.material.SetTexture("_DirtMask", _templateDirtMask);
         }
 
+        private IEnumerator CalculateCleanedPercentageRoutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(calculationInterval);
+
+                if (_isDirty && !_calculationStopped)
+                {
+                    CalculateCleanedPercentage();
+
+                    if (_calculationStopped)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void CalculateCleanedPercentage()
+        {
+            int cleanedPixelCount = 0;
+
+            for (int i = 0; i < _dirtMaskPixels.Length; i++)
+            {
+                if (_dirtMaskPixels[i].r == 0 && _dirtMaskPixels[i].g == 0 && _dirtMaskPixels[i].b == 0)
+                {
+                    cleanedPixelCount++;
+                }
+            }
+
+            float cleanedPercentage = (float)cleanedPixelCount / _totalPixels * 100;
+            Debug.Log($"Cleaned Area: {cleanedPercentage:F2}%");
+
+            if (cleanedPercentage >= 100f)
+            {
+                _calculationStopped = true;
+                Debug.Log("Cleaning completed. Calculation stopped.");
+            }
+        }
+
         public override void ResetActor()
         {
             // Implement reset logic here if necessary
@@ -93,7 +146,6 @@ namespace DefaultNamespace
             [NativeDisableParallelForRestriction] public NativeArray<Color32> dirtMaskPixels;
             [ReadOnly] public NativeArray<Color32> brushPixels;
             public int brushWidth;
-            public int brushHeight;
             public int maskWidth;
             public int pixelXOffset;
             public int pixelYOffset;
